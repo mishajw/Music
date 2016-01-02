b73fc9b7db4f509e8f34ff27bbd758cdc9c3d64d
Misha wagner <mishajw@gmail.com>
2016-01-02 00:34:31 +0000

    Fixed small errors, added frequencies

--- main.cpp
+++ main.cpp
@@ -1,21 +1,33 @@
 #include <iostream>
+#include <map>
 #include <math.h>
 
 #define SAMPLE_RATE 8000.0
 
 using namespace std;
 
-double defaultLength = SAMPLE_RATE;
+double defaultLength = SAMPLE_RATE * 0.5;
 unsigned int maxInt = (unsigned int) pow(2, 32);
 
 void sinePlay(double frequency);
 
 int main() {
-//    for (double i = 440; i < 500; i += 10) {
-//        sinePlay(i);
-//    }
+    map<string, double> freqs;
+    freqs["a"] = 440.00;
+    freqs["b"] = 493.88;
+    freqs["c"] = 523.25;
+    freqs["d"] = 587.33;
+    freqs["e"] = 659.25;
+    freqs["f"] = 698.46;
+    freqs["g"] = 783.99;
 
-    sinePlay(440);
+    sinePlay(freqs["a"]);
+    sinePlay(freqs["b"]);
+    sinePlay(freqs["c"]);
+    sinePlay(freqs["d"]);
+    sinePlay(freqs["e"]);
+    sinePlay(freqs["f"]);
+    sinePlay(freqs["g"]);
 
     return 0;
 }
@@ -30,8 +42,7 @@ void sinePlay(double frequency) {
     cerr << incr * SAMPLE_RATE << endl;
 
     for (int i = 0; i < defaultLength; i ++) {
-        double scaledSin = sin(fmod(incred, M_PI));
-        unsigned int toPlay = (unsigned int) (scaledSin * maxInt * 0.1);
+        int toPlay = (int) (sin(fmod(incred, M_PI)) * maxInt * 0.1);
 
         cout << (char) ((toPlay >> 24) & 0xFF) <<
                 (char) ((toPlay >> 16) & 0xFF) <<
