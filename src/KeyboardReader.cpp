40dddc33d40534009b9efe9f33605fcf61b37691
Misha wagner <mishajw@gmail.com>
2016-01-03 23:24:05 +0000

    Started reading from keyboard

--- src/KeyboardReader.cpp
+++ src/KeyboardReader.cpp
@@ -3,3 +3,52 @@
 //
 
 #include "KeyboardReader.h"
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <iostream>
+
+using namespace std;
+
+void KeyboardReader::run() {
+  struct input_event ev[64];
+  int fd, size = sizeof (struct input_event);
+  string device = "/dev/input/event0";
+
+  //Open Device
+  if ((fd = open (device.c_str(), O_RDONLY)) == -1) {
+    cerr << "Is not a vaild device." << endl;
+    return;
+  }
+
+  while (true) {
+    if (read (fd, ev, (size_t) (size * 64)) < size) {
+      cerr << "Couldn't read from device" << endl;
+    }
+
+    input_event event = ev[1];
+    handleEvent(event);
+
+    if (event.code == 1) {
+      break;
+    }
+  }
+}
+
+void KeyboardReader::handleEvent(input_event &event) {
+  int type = event.value;
+  int keyCode = event.code;
+
+  cerr << keyCode << endl;
+
+  switch (type) {
+    case 0:
+      pressedKeys.erase(keyCode);
+      break;
+    case 1:
+      pressedKeys.insert(keyCode);
+      break;
+    default:
+      break;
+  }
+}
--- src/KeyboardReader.h
+++ src/KeyboardReader.h
@@ -5,9 +5,16 @@
 #ifndef MUSIC_KEYBOARDREADER_H
 #define MUSIC_KEYBOARDREADER_H
 
+#include <set>
+#include <linux/input.h>
 
 class KeyboardReader {
+public:
+    void run();
 
+private:
+    std::set<int> pressedKeys;
+    void handleEvent(input_event& event);
 };
 
 
