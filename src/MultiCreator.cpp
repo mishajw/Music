1f02b5afb10e40a32e6d6fde1d6695c9e793a426
Misha wagner <mishajw@gmail.com>
2016-01-09 17:26:52 +0000

    Removed useless comments

--- src/MultiCreator.cpp
+++ src/MultiCreator.cpp
@@ -14,19 +14,9 @@ MultiCreator::MultiCreator(std::set<MusicCreator *> &children) {
 int MultiCreator::getNext() {
   int total = 0;
 
-//  for (unsigned int i = 0; i < children.size(); i++) {
-//    total += children.at(i)->getScaledNext();
-//  }
-
   for (auto const c : children) {
     total += c->getScaledNext() / children.size();
   }
-
-//  std::cout << total << std::endl;
-//  if (children.size() > 0) {
-//    total /= children.size();
-//  }
-//  std::cout << total << std::endl;
 
   return total;
 }
